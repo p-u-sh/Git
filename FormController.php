dc4712da09130bd8c151d843c22fd7efa5714ba6
Phila Sithole <phila@onswaziline.com>
2024-03-19 16:31:41 +0200

    updated FormController Version

--- FormController.php
+++ FormController.php
@@ -41,18 +41,36 @@ class FormController extends Controller
         $filePath3 = $directory . '/' . $fileName3;
         $request->id_card->move($directory, $fileName3);
 
-
-
         $fileName4 = $app->id . '_payslip_' . $request->payslip->getClientOriginalName();
         $filePath4 = $directory . '/' . $fileName4;
         $request->payslip->move($directory, $fileName4);
 
+        $fileName5 = $app->id . '_proof_of_residence_' . $request->proof_of_residence->getClientOriginalName();
+        $filePath5 = $directory . '/' . $fileName5;
+        $request->proof_of_residence->move($directory, $fileName5);
+
+        $fileName6 = $app->id . '_residence_confirm_' . $request->residential_address->getClientOriginalName();
+        $filePath6 = $directory . '/' . $fileName6;
+        $request->residential_address->move($directory, $fileName6);
+
+        $fileName7 = $app->id . '_owner_id_' . $request->owner_id->getClientOriginalName();
+        $filePath7 = $directory . '/' . $fileName7;
+        $request->owner_id->move($directory, $fileName7);
+
+        $fileName8 = $app->id . '_image_' . $request->photo->getClientOriginalName();
+        $filePath8 = $directory . '/' . $fileName8;
+        $request->photo->move($directory, $fileName8);
 
         $app_update = Application::where('id', $app->id)
             ->update([
                 'membership_form' => $fileName,
                 'stop_order' => $fileName2,
                 'id_card' => $fileName3,
+                'payslip' => $fileName4,
+                'proof_residence' => $fileName5,
+                'residence_confirmation' => $fileName6,
+                'owner_residence_id' => $fileName7,
+                'passport_photo' => $fileName8,
             ]);
     }
 
